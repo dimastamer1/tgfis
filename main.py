7230325fe58f8b223569f6a41110bf8464424e20
dimastamer1 <dimaolx52528@gmail.com>
2025-09-01 11:19:13 +0300

    bnfkb webappecaudor

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from datetime import datetime
 
 load_dotenv()
 API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH"))
+API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
 
